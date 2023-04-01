1e0cd18580be4c0866cce840331673a8cccb67fe
andre <lm>
2023-04-01 14:16:10 +0300

    main1

--- app/src/main/java/com/example/hakatongdlapp/MainActivity.java
+++ app/src/main/java/com/example/hakatongdlapp/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.hakatongdlapp;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.SyncStateContract;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(MainActivity.this, Novel.class);
-        intent.putExtra("tag", 1);
-        startActivityForResult(intent, Constants.REQUEST_CODE_ONE);
-
+    }
+    public void startLK(View v){
+        Intent intention = new Intent(this,lk.class);
+        startActivity(intention);
     }
 }
