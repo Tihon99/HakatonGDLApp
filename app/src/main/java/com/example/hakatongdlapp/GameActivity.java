5f4bf9d1b52f75c160a366df5e8cfb96200a68ad
andre <lm>
2023-04-01 18:44:45 +0300

    game1

--- app/src/main/java/com/example/hakatongdlapp/GameActivity.java
+++ app/src/main/java/com/example/hakatongdlapp/GameActivity.java
@@ -2,13 +2,38 @@ package com.example.hakatongdlapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class GameActivity extends AppCompatActivity {
-
+    ImageButton ImageButtonotvet1;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        final float[] i = {100};
+        ImageButtonotvet1 = (ImageButton)findViewById(R.id.imageButton12);
+        ImageButtonotvet1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Runnable task = () -> {
+                    for (int k = 0; k < 100; k++) {
+                        try {
+                            Thread.sleep(10);
+                        } catch (InterruptedException ie) {
+                            System.out.println();
+                        }
+                        ImageButtonotvet1.setTranslationX(i[0]);
+                        i[0] += 2;
+                    }
+
+                };
+            }
+        });
     }
 }
