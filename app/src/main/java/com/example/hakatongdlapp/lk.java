0c43fcef302b1f1a64cc21ac8243f35c6297d4d4
andre <lm>
2023-04-01 13:52:36 +0300

    lk

---
+++ app/src/main/java/com/example/hakatongdlapp/lk.java
@@ -0,0 +1,14 @@
+package com.example.hakatongdlapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lk extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lk);
+    }
+}
