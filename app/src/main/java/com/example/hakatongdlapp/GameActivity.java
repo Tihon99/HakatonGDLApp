ca5b213d3cf9b20e1c6ba0e2b8f48826e383a556
andre <lm>
2023-04-01 23:09:29 +0300

    lkrego2

--- app/src/main/java/com/example/hakatongdlapp/GameActivity.java
+++ app/src/main/java/com/example/hakatongdlapp/GameActivity.java
@@ -6,34 +6,39 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity {
     ImageButton ImageButtonotvet1;
+    int flag,combo_count,sum;
+    private TextView Score;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        final float[] i = {100};
-        ImageButtonotvet1 = (ImageButton)findViewById(R.id.imageButton12);
-        ImageButtonotvet1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        sum=0;
+        combo_count=1;
+        flag=1;
 
-                Runnable task = () -> {
-                    for (int k = 0; k < 100; k++) {
-                        try {
-                            Thread.sleep(10);
-                        } catch (InterruptedException ie) {
-                            System.out.println();
-                        }
-                        ImageButtonotvet1.setTranslationX(i[0]);
-                        i[0] += 2;
-                    }
+    }
 
-                };
-            }
-        });
+
+
+    public void klickotvet(View view) {
+
+        if(flag==1){
+            combo_count= combo_count*2;
+            sum+=combo_count;
+            Score=findViewById(R.id.score);
+            Score.setText(String.valueOf(sum));
+        }
+        else{
+            combo_count=1;
+            Toast.makeText(this,"Ответ не верный",Toast.LENGTH_SHORT).show();
+        }
     }
 }
